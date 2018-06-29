c2b257113f3e77b195e728ef935a8c1cb921fffd
John Huynh <jhnhnyh9@gmail.com>
2018-06-29 00:01:42 -0700

    addressing issue 4

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Navigation = ({onRouteChange,isSignedIn}) => {
+const Navigation = ({ onRouteChange,isSignedIn, route }) => {
     return (
         (isSignedIn)
             ? 
@@ -8,10 +8,15 @@ const Navigation = ({onRouteChange,isSignedIn}) => {
                     <p onClick={() => onRouteChange('signout')} className='f3 link dim black underline pa3 pointer'>Sign Out</p>
                 </nav>
             :
-                <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
-                    <p onClick={() => onRouteChange('signin')} className='f3 link dim black underline pa3 pointer'>Sign In</p>
-                    <p onClick={() => onRouteChange('register')} className='f3 link dim black underline pa3 pointer'>Register</p>
-                </nav>
+            (route === 'signin')
+                ?
+                    <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+                        <p onClick={() => onRouteChange('register')} className='f3 link dim black underline pa3 pointer'>Register</p>
+                    </nav>
+                :
+                    <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+                        <p onClick={() => onRouteChange('signin')} className='f3 link dim black underline pa3 pointer'>Sign In</p>
+                    </nav>
     );
 }
 
